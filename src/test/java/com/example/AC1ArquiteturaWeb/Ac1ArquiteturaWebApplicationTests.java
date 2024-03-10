0f00e7af8d1ece239db9ec0ef0aaf7896c0c77af
Nicolas Navas Segato <ninavassegato@gmail.com>
2024-03-10 14:17:04 -0300

    Initial commit - Primeira alteração (Aula 03)

---
+++ src/test/java/com/example/AC1ArquiteturaWeb/Ac1ArquiteturaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AC1ArquiteturaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ac1ArquiteturaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
