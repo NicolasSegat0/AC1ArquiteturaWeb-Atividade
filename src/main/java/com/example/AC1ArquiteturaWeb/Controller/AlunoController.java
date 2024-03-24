34b202d64630f35c18487598f45b00c29527998c
Nicolas Navas Segato <ninavassegato@gmail.com>
2024-03-24 11:02:23 -0300

    Aula05
    Alterações realizadas para a aula de número  05.

--- src/main/java/com/example/AC1ArquiteturaWeb/Controller/AlunoController.java
+++ src/main/java/com/example/AC1ArquiteturaWeb/Controller/AlunoController.java
@@ -1,36 +1,39 @@
 package com.example.AC1ArquiteturaWeb.Controller;
 
 import com.example.AC1ArquiteturaWeb.Model.Aluno;
-import com.example.AC1ArquiteturaWeb.Repository.AlunoRepository;
+import com.example.AC1ArquiteturaWeb.Repository.AlunoRepositoryImpl;
+import com.example.AC1ArquiteturaWeb.Service.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class AlunoController {
-    @Autowired
-    private AlunoRepository repository;
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
 
     @GetMapping("/alunos")
     public List<Aluno> getAlunos() {
-        return repository.findAll();
+        return alunoService.findAll();
     }
 
     @GetMapping("/alunos/{nomeAluno}")
     public Aluno getOne(@PathVariable ("nomeAluno") String nomeAluno) {
-        return repository.findByNomeAluno(nomeAluno);
+        return alunoService.findByNomeAluno(nomeAluno);
     }
 
     @DeleteMapping("/alunos/{id}")
     public void deleteAluno(@PathVariable("id") Long id) {
-        repository.deleteById(id);
+        alunoService.deleteById(id);
     }
 
     @PostMapping("/alunos")
     public void postAluno(@RequestBody Aluno aluno) {
-        repository.save(aluno);
+        alunoService.save(aluno);
     }
 
     @PutMapping("/alunos/{id}")
